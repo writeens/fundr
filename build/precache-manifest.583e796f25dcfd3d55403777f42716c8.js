57ed017e0cee15b20c1bd15a0cd3f7415014685c
writeens <44687555+writeens@users.noreply.github.com>
2019-06-08 20:59:40 +0100

    show build

---
+++ build/precache-manifest.583e796f25dcfd3d55403777f42716c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a30b39934bb0cb56e45c787be68a67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01df41bdef0e593ea525",
+    "url": "/static/css/main.7981dae0.chunk.css"
+  },
+  {
+    "revision": "4536fa795d7142e6519a",
+    "url": "/static/js/2.4672aee1.chunk.js"
+  },
+  {
+    "revision": "01df41bdef0e593ea525",
+    "url": "/static/js/main.09a7d982.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
